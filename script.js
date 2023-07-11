c47585b6dbf989d290424969825bdd8a8b7cfcf5
Tansuluuu <tynalievatan@gmail.com>
2023-07-11 16:16:46 +0600

    made js second work

---
+++ script.js
@@ -0,0 +1,88 @@
+// let a='Tansuluu'
+// let b ='Tynalieva'
+// let c=a.slice (1)
+// let d=b.slice(1)
+// let e=a.slice(a.length -3) .toUpperCase()
+// let f=b.slice(a.length -3) .toUpperCase()
+
+// console.log(b[b.length -1] +c, a[a.length -1] +d)
+
+// console.log(b[b.length -1] +c.slice(0, c.slice(0, c.length-3) +e, a[0]+ d.slice(0, d.length-3) +f))
+// console.log(e)
+
+// let a='lorem ipsum dolor sit amet'
+// let b=a.slice(a.lastIndexOf('s'), a.lastIndexOf('s') + 3)
+// let c
+//
+// console.log(b)
+
+// 1
+// let a = 'hello'
+// let b = 'Ivan'
+// let c=a[0].toUpperCase() + a.slice(1)
+// let d=b[0].toLowerCase() + b.slice(1)
+//
+// console.log(c +" "+ d)
+
+// 2
+// let a = 'lorem ipsum dolor'
+// let b=a.length
+// console.log(b)
+
+// 3
+// let a = 'lorem ipsum dolor sit amet'
+// let b=a.slice(a.lastIndexOf('s'), a.lastIndexOf('s') +3)
+// let c=a.slice(0, a.lastIndexOf('r')+1 )
+// let d=a.slice(a.lastIndexOf( 'a' ))
+//
+// console.log(b +' '+ c + ' '+ d )
+
+//4
+// let a = 'children'
+// let b=a.toUpperCase()
+// let c=a[7].toLowerCase()
+// console.log(b + c)
+
+// 5
+// let a = 'boss'
+// let b=a.slice(2) +a[1].toLowerCase()
+// console.log(a[0]+b)
+
+// 6
+// let a = 'sunday'
+// console.log(a[3])
+
+// 7
+// let a = 'hello'
+// let c= a.replace('ll','ks')
+// console.log(c)
+
+// 8
+// let a = 'user'
+// let b = 'new'
+// let c=a.slice(0,2)
+// console.log(b + ' '+ c )
+
+// 9
+// let a = 'monday'
+// let b = 'day'
+// let c=a.slice(0,3)
+// console.log(b + b +c)
+
+// 10
+// let a = 'HELLO'
+// console.log(a[0].toLowerCase() + a[1].toUpperCase() + a[2].toLowerCase() + a[3].toUpperCase() + a[4].toLowerCase())
+
+// 11
+// let a = 5
+// let b = '400'
+// console.log(b.replace('0',a))
+
+// 12
+// let a = 'monkey'
+// let b = 'banana'
+// console.log(a[0]+b.slice(1,4)+ a[3])
+
+
+
+
